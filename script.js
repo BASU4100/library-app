7919734a43bb5779fd3ffe279cffa2d7a279eb66
Vivek Kumar <vk13114416@gmail.com>
2025-07-25 11:58:52 +0000

    Added few usefull functions. Updated the content and the UI of the libray app.

---
+++ script.js
@@ -0,0 +1,20 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.id = crypto.randomUUID();
+    this.info = function () {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ` + (this.read?"read already.":"not read yet.");
+    }
+}
+
+function addBookToLibrary() {
+    var title;
+    var author;
+    var pages;
+    var read;
+    var newBook = new Book(title, author, pages, read);
+}
